95a742ca52b6557973965351874c58397c1a5d09
souravdas090300 <souravdas090300@gmail.com>
2026-02-06 10:12:29 +0100

    Create amp.d.ts

---
+++ src/types/amp.d.ts
@@ -0,0 +1,20 @@
+import React from 'react'
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'amp-img': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        src?: string
+        width?: string | number
+        height?: string | number
+        layout?: string
+        alt?: string
+      }
+    }
+  }
+}
+
+export {}
